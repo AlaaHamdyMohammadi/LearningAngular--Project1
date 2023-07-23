ff61eeb61eccb53e6a747618833e3559d94333cf
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-23 18:06:51 +0200

    CustomPipes

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,12 +8,13 @@ import { Component } from '@angular/core';
     <h2>{{ title }}</h2>
     <!-- <button class="btn btn-primary" (click)="onSave($event)">Save</button> -->
     <!-- <input #name (keyup.enter)="onKeyUp(name.value)" /> -->
-    <h2>{{course.title | uppercase}}</h2>
-    <h2>{{course.rating | number: '1.1-2'}}</h2>
-    <h2>{{course.students | number}}</h2>
-    <h2>{{course.price | currency}}</h2>
-    <h2>{{course.releaseDate | date}}</h2>
-
+    <h2>{{ course.title | uppercase }}</h2>
+    <h2>{{ course.rating | number : '1.1-2' }}</h2>
+    <h2>{{ course.students | number }}</h2>
+    <h2>{{ course.price | currency }}</h2>
+    <h2>{{ course.releaseDate | date }}</h2>
+    <h1>{{ text | summary }}</h1>
+    
   `,
 })
 export class CoursesComponent {
@@ -24,10 +25,12 @@ export class CoursesComponent {
   course = {
     title: 'learning Angular',
     rating: 4.9758,
-    students:2458,
+    students: 2458,
     price: 190.99,
-    releaseDate: new Date(2023,2 ,5),
-  }
+    releaseDate: new Date(2023, 2, 5),
+  };
+
+  text = `Lorem ipsum dolor sit amet consectetur adiplllllllllllllllllllllllll corrupti?`;
 
   constructor(service: CoursesService) {
     //let service = new CoursesService();
