044096cdc8e2cd3f43d3dbbcfdeb625338278fa5
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-23 16:09:22 +0200

    StartBinding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,17 +5,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses',
   template: `
-    <h2>{{title}}</h2>
+    <h2>{{ title }}</h2>
+    <!-- <button [style.backgroundColor]="isActive ? 'blue' : 'green' "]>click</button> -->
     <ul>
-      <li *ngFor="let course of courses">{{course}}</li>
+      <li *ngFor="let course of courses">{{ course }}</li>
     </ul>
-    `,
+    <img [src]="img" alt="" />
+  `,
 })
 export class CoursesComponent {
+  [x: string]: any;
   title = 'List of courses';
+  isActive = true;
+  img = './../assets/features-03.jpg';
   courses: any;
 
-  constructor(service: CoursesService){
+  constructor(service: CoursesService) {
     //let service = new CoursesService();
     this.courses = service.getCourses();
   }
